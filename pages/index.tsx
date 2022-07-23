a7ff5328302b9a79ca5d74ebc67abdbda1a685a5
Bogdan <bogdanmc96@gmail.com>
2022-07-23 03:38:58 +0300

    Deleted some junk files and finished first version of homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,29 @@
 import type { NextPageWithLayout } from './_app'
 import { ReactElement } from 'react'
-import DarkModeToggle from '../components/DarkModeToggle'
 import Layout from '../components/Layout'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+import Avatar from '../components/Avatar'
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Home: NextPageWithLayout  = () => {
 
   return (
     <motion.div
-      className="mt-36"
+      className="mt-14"
       initial={{ opacity: 0, scale: 1 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.25 }}
     >
-      <div className="dark:text-primary flex flex-col text-center">
-        <h1 className='dark:text-primary font-bold'>Hey, I'm Bogdan Maftei</h1>
-        <h3 className='font-light mt-8 text-2xl'>Junior Software Developer</h3>
-      </div>      
+      <div className="dark:text-primary flex flex-col place-items-center text-center">
+        <Avatar />
+        <h1 className='font-bold mt-6 md:mt-10'>Hey, I'm Bogdan Maftei!</h1>
+        <h3 className='font-light mt-8 text-xl'>Junior Software Developer, experimental music producer and cats & dogs lover.</h3>
+        <ul className="flex flex-row p-6">
+          <li className="mx-2"><a href="https://www.facebook.com/maftei.bogdancosmin/" target="_blank"><FaFacebook size={25}/></a></li>
+          <li className="mx-2"><a href="https://github.com/bogdanc64" target="_blank"><FaGithub size={25}/></a></li>
+          <li className="mx-2"><a href="https://www.linkedin.com/in/bogdan-cosmin-maftei-412018215/" target="_blank"><FaLinkedin size={25}/></a></li>
+        </ul>
+      </div>
     </motion.div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
       fontFamily:{
         sans:['Manrope', 'sans-serif']
       },
+      boxShadow:{
+        avatar: '0 0 0 1px var(--color-border-avatar)',
+        avatar2: '0 0 0 1px var(--color-border-avatar-2)',
+        avatar3: '0 0 0 1px var(--color-border-avatar-3)',
+      },
       textColor: {
         accent: 'var(--color-text-accent)',
         primary: 'var(--color-text-primary)',
